ff6873bc5877dbb9b9c693ce3a8b68aa7745e6fd
JackBrens <574950259@qq.com>
2022-07-15 15:28:19 +0800

    修改base地址第二次

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: '/big-screen/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
