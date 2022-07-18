2e644b58fceb9edfa34fb37873f12a2930bea4c7
JackBrens <574950259@qq.com>
2022-07-18 17:25:30 +0800

    [feat] 新增项目启动自定义文字和打包完成后提示大小和时间

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// @ts-ignore
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import progress from 'vite-plugin-progress'
 import path from 'path'
+import viteBuildInfo from './build/info'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), viteBuildInfo(), progress()],
   base: '/big-screen/',
   resolve: {
     alias: {
